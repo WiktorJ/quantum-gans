d7d83007dfab5b6cb1d6f82a0c08ac7e87c26196
wiktorj <wiktor.jurasz@gmail.com>
2020-10-26 13:18:41 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quantum_gans',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/WiktorJ/quantum-gans',
+    license='',
+    author='WiktorJ',
+    author_email='',
+    description=''
+)
