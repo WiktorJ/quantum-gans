ccb0238bc980387fd6c9b3c1dab36d1feb7a89fb
wiktorj <wiktor.jurasz@gmail.com>
2021-01-22 15:52:19 +0100

    Added option to train without extra generator qubit

--- qsgenerator/qugans/circuits.py
+++ qsgenerator/qugans/circuits.py
@@ -3,16 +3,27 @@ import sympy
 
 
 def build_gan_circuits(generator_layers: int, discriminator_layers: int, data_bus_size: int,
-                       full_layer_labeling: bool = True, all_layers_labeling: bool = False):
-    qubits = cirq.GridQubit.rect(1, 3 + data_bus_size)
+                       full_layer_labeling: bool = True, all_layers_labeling: bool = False,
+                       use_gen_label_qubit: bool = False):
+    total_size = data_bus_size + 2
+    if use_gen_label_qubit:
+        total_size += 1
+
+    qubits = cirq.GridQubit.rect(1, total_size)
     out_qubit, label_disc_qubit = qubits[0], qubits[1]
-    data_qubits = qubits[2:-1]
-    label_gen_qubit = qubits[-1]
+    if use_gen_label_qubit:
+        data_qubits = qubits[2:-1]
+        label_gen_qubit = qubits[-1]
+    else:
+        data_qubits = qubits[2:]
+        label_gen_qubit = None
     ls = sympy.symbols("l")
-    gen, gen_symbols = build_circuit(generator_layers, label_gen_qubit, data_qubits + [label_gen_qubit], ls, "g",
+    gen_qubits = data_qubits + ([label_gen_qubit] if label_gen_qubit else [])
+    gen, gen_symbols = build_circuit(generator_layers, label_gen_qubit, gen_qubits, ls, "g",
                                      full_layer_labeling, all_layers_labeling)
     disc, disc_symbols = build_circuit(discriminator_layers, label_disc_qubit,
-                                       [out_qubit, label_disc_qubit] + data_qubits, ls, "d", full_layer_labeling, all_layers_labeling)
+                                       [out_qubit, label_disc_qubit] + data_qubits, ls, "d", full_layer_labeling,
+                                       all_layers_labeling)
     return gen, gen_symbols, disc, disc_symbols, ls, data_qubits, out_qubit
 
 
@@ -25,7 +36,7 @@ def build_circuit(layers: int, label_qubit, data_qubits, label_symbol, data_qubi
         if i > 0 and not all_layers_labeling:
             circuit.append([_build_layer(None, data_qubits, None,
                                          symbols[i * layer_symbols_size: (i + 1) * layer_symbols_size],
-                                         full_layer_labeling)])
+                                         False)])
         else:
             circuit.append([_build_layer(label_qubit, data_qubits, label_symbol,
                                          symbols[i * layer_symbols_size: (i + 1) * layer_symbols_size],
@@ -38,16 +49,13 @@ def _build_layer(label_qubit, data_qubits, label_symbol, data_symbols, full_laye
     assert len(data_symbols) == len(data_qubits) * 3 - 1
 
     # Add the label rotation
+    layer = cirq.Circuit()
+    if full_layer_labeling:
+        for data_qubit in data_qubits:
+            layer.append(cirq.rx(label_symbol).on(data_qubit))
     if label_qubit is not None and label_symbol is not None:
-        layer = cirq.Circuit()
-        if full_layer_labeling:
-            for data_qubit in data_qubits:
-                layer.append([cirq.rx(label_symbol).on(data_qubit)])
-        else:
-            layer = cirq.Circuit(
-                cirq.rx(label_symbol).on(label_qubit))
-    else:
-        layer = cirq.Circuit()
+        layer.append(
+            cirq.rx(label_symbol).on(label_qubit))
 
     # Define symbol iterator
     i = 0
