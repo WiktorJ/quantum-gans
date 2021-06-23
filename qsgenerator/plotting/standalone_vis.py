729eb6da414d2255c91c63798beab74934c1b009
wiktorj <wiktor.jurasz@gmail.com>
2021-06-23 09:20:04 +0200

    Progress chapter 4 and visualisations

---
+++ qsgenerator/plotting/standalone_vis.py
@@ -0,0 +1,76 @@
+from collections import defaultdict
+from pathlib import Path
+
+import neptune.new as neptune
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+sns.set()
+
+FIG_PATH_PREFIX = "/Users/wiktorjurasz/Studies/thesis/paper/figures"
+
+id_prefix = "THES-"
+
+size_3_ids = [f"{id_prefix}{el}" for el in [64, 72, 73, 74, 75]]
+
+metadata = [
+    {'ids': size_3_ids, 'dir': 'sqgans_size=3', 'width': 3}
+]
+
+colors = sns.color_palette("husl", 3)
+prefix_to_metrics_metadata = {
+    'fidelity_g': {'name': 'fidelity', 'color': colors[0]},
+    'prob_real_real': {'name': 'probability real as real', 'color': colors[1]},
+    'prob_fake_real': {'name': 'probability generated as real', 'color': colors[2]}
+}
+
+
+def get_data_for_id(rid):
+    return neptune.init(
+        project='wiktor.jurasz/thesis',
+        api_token=None,  # put the token in NEPTUNE_API_TOKEN env variable
+        run=rid)
+
+
+def get_data(ids, param_prefixes):
+    data = defaultdict(list)
+    for rid in ids:
+        run = get_data_for_id(rid)
+        for param_prefix in param_prefixes:
+            field_name = [k for k in run.get_structure()['logs'].keys() if k.startswith(param_prefix)][0]
+            res = run[f"logs/{field_name}"].fetch_values()
+            res = res[['value']].to_numpy().flatten()
+            res = [el for i, el in enumerate(res) if i % 2 == 0]
+            data[param_prefix].append(res)
+    return data
+
+
+def plot_metric(metric, data_name, color, width):
+    fig, ax = plt.subplots()
+    mean = np.mean(np.stack(metric), axis=0)
+    mi = np.min(np.stack(metric), axis=0)
+    ma = np.max(np.stack(metric), axis=0)
+    std = np.std(np.stack(metric), axis=0)
+    ax.plot(list(range(len(metric[0]))), mean, c=color)
+    ax.fill_between(list(range(len(metric[0]))), mi, ma, alpha=0.3, facecolor=color)
+    # ax.fill_between(list(range(len(metric[0]))), mean - std, mean + std, alpha=0.3, facecolor=color)
+    ax.set_xlabel("epoch", fontsize=18)
+    ax.set_ylabel(data_name, fontsize=18)
+    ax.set_ylim(0, 1)
+    fig.suptitle(f"real input width = {width}")
+    plt.show()
+    return fig
+
+
+for meta in metadata:
+    data = get_data(meta['ids'], prefix_to_metrics_metadata.keys())
+    for prefix, metric in data.items():
+        name = prefix_to_metrics_metadata[prefix]['name']
+        fig = plot_metric(metric,
+                          name,
+                          prefix_to_metrics_metadata[prefix]['color'],
+                          meta['width'])
+        Path(f"{FIG_PATH_PREFIX}/{meta['dir']}").mkdir(exist_ok=True)
+        fig.savefig(f"{FIG_PATH_PREFIX}/{meta['dir']}/{name.replace(' ', '_')}.png")
